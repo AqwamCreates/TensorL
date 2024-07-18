d294f99bdf1887ab7b0db87b7469f8a3ae7e5589
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-07-18 11:40:17 +0800

    Update TensorL_Table.lua

--- TensorL_Table.lua
+++ TensorL_Table.lua
@@ -1212,6 +1212,42 @@ function AqwamTensorLibrary:extract(tensor, originDimensionIndexArray, targetDim
 		error(errorString)
 
 	end
+
+	if (outOfBoundsTargetIndexArraySize > 0) then
+
+		local errorString = "Attempting to set an target dimension index that is out of bounds for dimension at "
+
+		for i, index in ipairs(outOfBoundsTargetIndexArray) do
+
+			errorString = errorString .. index
+
+			if (i < outOfBoundsTargetIndexArraySize) then errorString = errorString .. ", " end
+
+		end
+
+		errorString = errorString .. "."
+
+		error(errorString)
+
+	end
+
+	if (falseBooleanIndexArraySize > 0) then
+
+		local errorString = "The origin dimension index is larger than the target dimension index for dimensions at "
+
+		for i, index in ipairs(outOfBoundsOriginIndexArray) do
+
+			errorString = errorString .. index
+
+			if (i < falseBooleanIndexArraySize) then errorString = errorString .. ", " end
+
+		end
+
+		errorString = errorString .. "."
+
+		error(errorString)
+
+	end
 	
 	return extract(tensor, originDimensionIndexArray, targetDimensionIndexArray)
 	
