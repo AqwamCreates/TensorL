6c9d27b5b5b75acbbc9f516a4fd904fb1faabf8a
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-08-25 16:49:23 +0800

    Update TensorL_Object_Recurvsive_Efficient.lua

--- TensorL_Object_Recurvsive_Efficient.lua
+++ TensorL_Object_Recurvsive_Efficient.lua
@@ -2367,15 +2367,9 @@ local function incrementDimensionIndexArray(dimensionSizeArray, dimensionIndexAr
 
 		dimensionIndexArray[i] = dimensionIndexArray[i] + 1
 
-		if (dimensionIndexArray[i] > dimensionSizeArray[i]) then
+		if (dimensionIndexArray[i] <= dimensionSizeArray[i]) then break end
 
-			dimensionIndexArray[i] = 1
-
-		else
-
-			break
-
-		end
+		dimensionIndexArray[i] = 1
 
 	end
 
