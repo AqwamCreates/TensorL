d9e65f3533597c9216ad1249db2c4f79544bd347
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-09-03 07:07:20 +0800

    Update TensorL_Object_Mixed.lua

--- TensorL_Object_Mixed.lua
+++ TensorL_Object_Mixed.lua
@@ -1918,6 +1918,66 @@ function AqwamTensorLibrary:unaryMinus(...)
 
 end
 
+function AqwamTensorLibrary:logarithm(...)
+
+	local functionToApply = math.log
+
+	local resultTensor
+
+	if (self.tensor) then
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, self, ...)
+
+	else
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, ...)
+
+	end
+
+	return AqwamTensorLibrary.new(resultTensor)
+
+end
+
+function AqwamTensorLibrary:exponent(...)
+
+	local functionToApply = math.exp
+
+	local resultTensor
+
+	if (self.tensor) then
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, self, ...)
+
+	else
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, ...)
+
+	end
+
+	return AqwamTensorLibrary.new(resultTensor)
+
+end
+
+function AqwamTensorLibrary:power(...)
+
+	local functionToApply = math.power
+
+	local resultTensor
+
+	if (self.tensor) then
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, self, ...)
+
+	else
+
+		resultTensor = applyFunctionOnMultipleTensors(functionToApply, ...)
+
+	end
+
+	return AqwamTensorLibrary.new(resultTensor)
+
+end
+
 function AqwamTensorLibrary:__tostring()
 
 	return self:generateTensorString()
