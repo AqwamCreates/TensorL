2e88c392e744d26cb2c9518ee395d912ab9a55b0
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2024-09-11 13:43:47 +0800

    Update TensorL_Table_Nested_Efficient_IPairs.lua

--- TensorL_Table_Nested_Efficient_IPairs.lua
+++ TensorL_Table_Nested_Efficient_IPairs.lua
@@ -2853,6 +2853,12 @@ function AqwamTensorLibrary:concatenate(tensor1, tensor2, dimension)
 
 end
 
+function AqwamTensorLibrary:unaryMinus(...)
+
+	return applyFunctionOnMultipleTensors(function(a) return (-a) end, ...)
+
+end
+
 function AqwamTensorLibrary:add(...)
 
 	return applyFunctionOnMultipleTensors(function(a, b) return (a + b) end, ...)
