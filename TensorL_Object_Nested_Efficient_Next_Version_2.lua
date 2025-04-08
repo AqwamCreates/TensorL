bc4b4e3c547abe1898f397127cb4efdd58de7775
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-04-08 15:07:10 +0800

    Update TensorL_Object_Nested_Efficient_Next_Version_2.lua

--- TensorL_Object_Nested_Efficient_Next_Version_2.lua
+++ TensorL_Object_Nested_Efficient_Next_Version_2.lua
@@ -3371,4 +3371,68 @@ function AqwamTensorLibrary:permute(dimensionArray)
 
 end
 
+local function flip(tensor,  dimensionSizeArray, numberOfDimensions, currentDimension, dimension)
+
+	local nextDimension = currentDimension + 1
+
+	local resultTensor = {}
+
+	if (currentDimension < numberOfDimensions) and (currentDimension == dimension) then
+
+		for i, subTensor in next, tensor do
+
+			local resultSubTensor = flip(subTensor,  dimensionSizeArray, numberOfDimensions, nextDimension, dimension)
+
+			table.insert(resultTensor, 1, resultSubTensor)
+
+		end
+
+	elseif (currentDimension < numberOfDimensions) and (currentDimension ~= dimension) then
+
+		for i, subTensor in next, tensor do
+
+			resultTensor[i] = flip(subTensor,  dimensionSizeArray, numberOfDimensions, nextDimension, dimension)
+
+		end
+
+	elseif (currentDimension == numberOfDimensions) and (currentDimension == dimension) then
+
+		for i, value in next, tensor do
+
+			table.insert(resultTensor, 1, value)
+
+		end
+
+	elseif (currentDimension == numberOfDimensions) and (currentDimension ~= dimension) then
+
+		for i, value in next, tensor do
+
+			resultTensor[i] = value
+
+		end
+
+	end
+
+	return resultTensor
+
+end
+
+function AqwamTensorLibrary:flip(tensor, dimension)
+
+	if (type(dimension) ~= "number") then error("Invalid dimension.") end
+
+	local dimensionSizeArray = AqwamTensorLibrary:getDimensionSizeArray(tensor)
+
+	local numberOfDimensions = #dimensionSizeArray
+
+	if (dimension <= 0) then error("The dimension cannot be less than or equal to zero.") end
+
+	if (dimension > numberOfDimensions) then error("The dimension is greater than the tensor's number of dimensions.") end
+	
+	local resultTensor = flip(tensor,  dimensionSizeArray, numberOfDimensions, 1, dimension)
+
+	return AqwamTensorLibrary.new(resultTensor)
+
+end
+
 return AqwamTensorLibrary
