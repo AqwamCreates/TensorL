c817065de29cbd658dc5e9ecb5a23d156cb47c4d
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-04-08 13:03:50 +0800

    Update TensorL_Table_Nested_Efficient_Version_3.lua

--- TensorL_Table_Nested_Efficient_Version_3.lua
+++ TensorL_Table_Nested_Efficient_Version_3.lua
@@ -3523,4 +3523,66 @@ function AqwamTensorLibrary:permute(tensor, dimensionArray)
 
 end
 
+local function flip(tensor,  dimensionSizeArray, numberOfDimensions, currentDimension, dimension)
+
+	local resultTensor = {}
+
+	local nextDimension = currentDimension + 1
+
+	local currentDimensionSize = dimensionSizeArray[currentDimension]
+
+	if (currentDimension < numberOfDimensions) and (currentDimension == dimension) then
+
+		for i = 1, currentDimensionSize, 1 do
+
+			resultTensor[i] = flip(tensor[(currentDimensionSize - i) + 1],  dimensionSizeArray, numberOfDimensions, nextDimension, dimension)
+
+		end
+
+	elseif (currentDimension < numberOfDimensions) and (currentDimension ~= dimension) then
+
+		for i = 1, currentDimensionSize, 1 do
+
+			resultTensor[i] = flip(tensor[i],  dimensionSizeArray, numberOfDimensions, nextDimension, dimension)
+
+		end
+
+	elseif (currentDimension == numberOfDimensions) and (currentDimension == dimension) then
+
+		for i = 1, currentDimensionSize, 1 do
+
+			resultTensor[i] = tensor[(currentDimensionSize - i) + 1]
+
+		end
+
+	elseif (currentDimension == numberOfDimensions) and (currentDimension ~= dimension) then
+
+		for i = 1, currentDimensionSize, 1 do
+
+			resultTensor[i] = tensor[i]
+
+		end
+
+	end
+
+	return resultTensor
+
+end
+
+function AqwamTensorLibrary:flip(tensor, dimension)
+
+	if (type(dimension) ~= "number") then error("Invalid dimension.") end
+
+	local dimensionSizeArray = AqwamTensorLibrary:getDimensionSizeArray(tensor)
+
+	local numberOfDimensions = #dimensionSizeArray
+
+	if (dimension <= 0) then error("The dimension cannot be less than or equal to zero.") end
+
+	if (dimension > numberOfDimensions) then error("The dimension is greater than the tensor's number of dimensions.") end
+
+	return flip(tensor,  dimensionSizeArray, numberOfDimensions, 1, dimension)
+
+end
+
 return AqwamTensorLibrary
